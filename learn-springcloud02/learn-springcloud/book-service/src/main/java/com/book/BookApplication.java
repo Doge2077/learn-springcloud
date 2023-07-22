6003c36819ff5b53b9e08c0a6496fc5af381fec7
Dog2077 <1002302241@qq.com>
2023-07-22 20:26:50 +0800

    Learn basic oauth

--- learn-springcloud02/learn-springcloud/book-service/src/main/java/com/book/BookApplication.java
+++ learn-springcloud02/learn-springcloud/book-service/src/main/java/com/book/BookApplication.java
@@ -2,8 +2,10 @@ package com.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
--- learn-springcloud02/learn-springcloud/user-service/src/main/java/com/user/UserApplication.java
+++ learn-springcloud02/learn-springcloud/user-service/src/main/java/com/user/UserApplication.java
@@ -2,8 +2,10 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
