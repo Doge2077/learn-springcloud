9af8e4af28f88bb09876d34206fdcfcc090c11ce
Dog2077 <1002302241@qq.com>
2023-07-23 09:28:09 +0800

    Learn shardingjdbc

---
+++ learn-sharedingjdbc/src/main/java/com/example/LearnSharedingjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSharedingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSharedingjdbcApplication.class, args);
+    }
+
+}
